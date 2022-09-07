a42b7222cb00771e98cc3b76ef298045d41b2814
Harley Johnson <hjohnson@uwm.com>
2022-09-07 10:44:54 -0400

    More setup?

---
+++ HarleeCreations.API/Controllers/ThemeController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using HarleeCreations.Crosscutting.Models.Theme;
+using Microsoft.AspNetCore.Cors;
+using HarleeCreations.Domain.Orchestrators;
+
+namespace HarleeCreations.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [EnableCors]
+    public class ThemeController : ControllerBase
+    {
+        private ThemeOrchestrator orchestrator;
+
+        public ThemeController()
+        {
+            orchestrator = new ThemeOrchestrator();
+        }
+
+        [HttpPost]
+        public void AddColorScheme(ColorSchemeModel model)
+        {
+            orchestrator.AddColorScheme(model);
+        }
+    }
+}
---
+++ HarleeCreations.Domain/Orchestrators/ThemeOrchestrator.cs
@@ -0,0 +1,40 @@
+﻿using HarleeCreations.Crosscutting.Dtos.Theme;
+using HarleeCreations.Crosscutting.Models.Theme;
+using HarleeCreations.Data.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HarleeCreations.Domain.Orchestrators
+{
+    public class ThemeOrchestrator
+    {
+        private ThemeRepository repository;
+        
+        public ThemeOrchestrator()
+        {
+            repository = new ThemeRepository();
+        }
+        public void AddColorScheme(ColorSchemeModel model)
+        {
+            ColorSchemeDto dto = new ColorSchemeDto()
+            {
+                UniqueName = model.UniqueName,
+                V100 = model.V100,
+                V200 = model.V200,
+                V300 = model.V300,
+                V400 = model.V400,
+                V500 = model.V500,
+                V600 = model.V600,
+                V700 = model.V700,
+                V800 = model.V800,
+                V900 = model.V900,
+            };
+
+            repository.AddColorScheme(dto);
+
+        }
+    }
+}
